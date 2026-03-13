9c8448385b6313d3d504fe99c7e4f18348727a3d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:56:55 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422811528/pages/about.tsx
@@ -0,0 +1,12 @@
+import Layout from '../layout';
+
+function About() {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    </Layout>
+  );
+}
+
+export default About;
