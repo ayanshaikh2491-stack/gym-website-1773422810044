f1f7dca3174bc1749f732582b2119398e181ea2a
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:56:55 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422811528/pages/membership.tsx
@@ -0,0 +1,12 @@
+import Layout from '../layout';
+
+function Membership() {
+  return (
+    <Layout>
+      <h1>Membership Plans</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    </Layout>
+  );
+}
+
+export default Membership;
